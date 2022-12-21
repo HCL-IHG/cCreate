d8ee9ac97bfd1c59dceea6a33425c64e7a9c836e
Parmy <hcl.ihg@gmail.com>
2022-12-21 20:36:57 +0000

    cookbook_file demo

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,24 @@
 # Recipe:: default
 #
 # Copyright:: 2022, The Authors, All Rights Reserved.
+#
+# Cookbook:: tCreate
+# Recipe:: default
+#
+# Copyright:: 2022, The Authors, All Rights Reserved.
+#
+# Cookbook:: fCreate
+# Recipe:: default
+#
+# Copyright:: 2022, The Authors, All Rights Reserved.
+#node['ipaddress']
+#node['memory']['total']
+
+#print statement I have 4 apples
+apple_count = 4
+puts "I have #{apple_count} apples"
+
+cookbook_file '/root/fCreate.txt' do
+  source 'cCreate.txt'
+end
+#include_recipe 'fUpdate::uRecipe'
